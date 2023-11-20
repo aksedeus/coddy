88c51fac8d757bc6bc98744d0d175a2f56ab1c38
aksedeus <116082819+aksedeus@users.noreply.github.com>
2023-11-20 16:48:17 +0300

    Add files via upload

---
+++ python/matrix/sprites.py
@@ -0,0 +1,18 @@
+from pygame.sprite import Sprite
+
+
+W, H = 800, 600
+
+
+class Symbol(Sprite):
+	def __init__(self, surf, center, group):
+		super().__init__()
+		self.image = surf
+		self.rect = surf.get_rect(center=center)
+		self.add(group)
+
+	def update(self):
+		if self.rect.y < H:
+			self.rect.y += 10
+		else:
+			self.kill()
